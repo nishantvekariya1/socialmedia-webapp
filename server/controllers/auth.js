6c5ba6d2c3463413132961a1e49770b2fee13870
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-01-11 14:48:23 +0530

    implement login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,3 +38,29 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "user does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    delete user.password;
+
+    res.status(200).json(token, user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import register from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -34,9 +35,11 @@ const storage = multer.diskStorage({
   },
 });
 
+const upload = multer({ storage });
+
 app.post("/auth/register", upload.single("picture"), register);
 
-const upload = multer({ storage });
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 mongoose
